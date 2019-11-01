989f8025ca667b93fab3daa410bb72614ba9eeeb
jeanshotjack <dshalenkova@gmail.com>
2019-11-01 14:33:43 -0400

    almost done style

--- assets/style/style.js
+++ assets/style/style.js
@@ -2,8 +2,10 @@
 $(document).on("scroll", (function() {
     if ($(document).scrollTop() > 60){  
         $("h1").addClass("sticky");
+        $("#what").css("font-size", "40px");
     }
     else{
         $("h1").removeClass("sticky");
+        $("#what").css("font-size", "70px");
     }
 }));
